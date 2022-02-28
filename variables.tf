3cea1aab34f2ef5aae117be7b4d92465eacfa5fa
Oleksandr Tabachuk <a.tabachuk@geniusee.com>
2022-02-28 16:24:51 +0200

    readme and fixes

--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
-variable "access_key" {
- 
-}
-
-variable "secret_key" {
-
-}
-
-variable "target" {
-
-}
+variable "access_key" {}
+variable "secret_key" {}
+variable "target" {}
 
 variable "how_many" {
   default = 250
